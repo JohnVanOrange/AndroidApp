997dc939e6a1204b77806463d54963a2cf89c32e
Cameron Bulock <cbulock@liquidweb.com>
2013-05-28 10:23:20 -0400

    new 2.0 series build of the app

--- gen/com/johnvanorange/cbulock/R.java
+++ gen/com/johnvanorange/cbulock/R.java
@@ -12,8 +12,9 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
-        public static final int splash=0x7f020001;
-        public static final int splash_image=0x7f020002;
+        public static final int icon=0x7f020001;
+        public static final int splash=0x7f020002;
+        public static final int splash_image=0x7f020003;
     }
     public static final class layout {
         public static final int main=0x7f030000;
